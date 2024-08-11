d8cce4615ef6f23c2dacf0d2f78af449bef756ef
RamaKrushna92 <ram.grpt@gmail.com>
2024-08-11 22:35:32 +0530

    module path updated in main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "VPC" {
-  source      = "C:/Users/Ramakrishna/Documents/TerraformProject/modules/vpc"
+  source      = "./modules/vpc"
   vpc_cidr    = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
 
 module "dev_SG" {
-  source   = "C:/Users/Ramakrishna/Documents/TerraformProject/modules/sg"
+  source   = "./modules/sg"
   vpc_id   = module.VPC.vpc_id
   vpc_cidr = var.vpc_cidr
 }
 
 module "ec2" {
-  source   = "C:/Users/Ramakrishna/Documents/TerraformProject/modules/ec2"
+  source   = "./modules/ec2"
   SG_ID    = module.dev_SG.dev_SG
   subnets  = module.VPC.subnet_ids
   key_name = "jenkins-key"
